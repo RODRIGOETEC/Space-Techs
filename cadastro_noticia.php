6f2ab1e77bd4c5aab17f19ed29f85d2c25511895
vinicius pereira <vinicius.sampaio4@etec.sp.gov.br>
2023-04-04 17:56:34 -0300

    cadastro_noticia

---
+++ cadastro_noticia.php
@@ -0,0 +1,5 @@
+Titulo da Noticia<br>
+<input type="text" name="nome_noticia" value="" maxlength="45"> <br> <br>
+
+Data da Noticia<br>
+<input type="text" name="data_noticia" value="" maxlength="45"> <br> <br>
