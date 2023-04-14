103d288bd37e7372cbfac2a1318467a4bcacfea2
RODRIGOETEC <rodrigo.guedes3@etec.sp.gov.br>
2023-04-14 15:13:09 -0300

    aula14

--- esenha.php
+++ esenha.php
@@ -1,1 +1,12 @@
-Recuperar Senha
+<form action="" method="post">
+<link rel="stylesheet" href="user.css">
+<div class="center-pag"> 
+    <h1>Recuperar Senha</h1>
+<div class="textfield">
+    <label for="nome_user">Nome<br></label>
+
+
+<input type="text" name="nome_user id="" placeholder="Nome"><br>
+</div>
+</div>
+</fotm
