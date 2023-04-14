53cfa0e9921e14e0c34369591869af2a7276bd0a
Vinicius Pereira Sampaio <vinicius.sampaio4@etec.sp.gov.br>
2023-04-14 15:29:15 -0300

    aula 14

--- cadastro_noticia.php
+++ cadastro_noticia.php
@@ -1,5 +1,15 @@
+<h1>Cadastro Noticias</h1>
+
 Titulo da Noticia<br>
 <input type="text" name="nome_noticia" value="" maxlength="45"> <br> <br>
 
 Data da Noticia<br>
-<input type="text" name="data_noticia" value="" maxlength="45"> <br> <br>
+<input id="date" type="date"><br> <br> 
+
+Fonte da Noticia<br>
+<input type="text" name="nome_noticia" value="" maxlength="45"> <br> <br> 
+
+Nome do Autor <br>
+<input type="text" name="nome_noticia" value="" maxlength="45"> <br> <br>
+
+<input type="submit" value="Pesquisar">
--- esenha.php
+++ esenha.php
@@ -3,10 +3,13 @@
 <div class="center-pag"> 
     <h1>Recuperar Senha</h1>
 <div class="textfield">
-    <label for="nome_user">Nome<br></label>
+    <label for="nome_user">Email<br></label>
+<input type="text" name="nome_user id="" placeholder="Email"><br> <br>
+<label for="nome_user">Codigo de Segurança<br></label>
+<input type="text" name="nome_user id="" placeholder="Codigo de Seguranca"><br>
+<label for="nome_user"><br>Numero<br></label>
+<input type="text" name="nome_user id="" placeholder="Numero"><br> <br>
 
-
-<input type="text" name="nome_user id="" placeholder="Nome"><br>
 </div>
 </div>
 </fotm
