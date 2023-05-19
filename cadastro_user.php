121476fb078270a2a7177aa22fa8b65ffdebedc0
jonathas luan <jonathas.araujo3@etec.sp.gov.br>
2023-05-19 14:41:17 -0300

    Alteração 19

--- cadastro_user.php
+++ cadastro_user.php
@@ -5,49 +5,49 @@
    <br>
    <div class="textfield">
     <label for="nome_user">Nome<br></label>
-<input type="text" name="nome_user id="" placeholder="Nome"><br>
+<input type="text" name ="nome_user" placeholder="Nome" required><br>
 </div>
 <br>
 <div class="textfield">
     <label for="sob_user">Sobrenome<br></label>
-<input type="text"  name="sob_user" id="" placeholder="Sobrenome"><br>
+<input type="text" name ="sob_user" placeholder="Sobrenome" required><br>
 </div>
 <br>
 <div class="textfield">
     <label for="name_user">Usuário<br></label>
-<input type="text"  name="name_user" id="" placeholder="Usuário"><br>
+<input type="text" name ="name_user" placeholder="Usuário" required><br>
 </div>
 <br>
 <div class="textfield">
     <label for="email_user">Email<br></label>
-<input type="text"  name="email_user" id="" placeholder="Email"><br>
+<input type="text" name ="email_user" placeholder="Email" required><br>
 </div>
 <br>
 <div class="textfield">
     <label for="cod_user">Código de Segurança de 6 digitos<br></label>
-<input type="text"  name="cod_user" id="" placeholder="EX:161384"  maxLength="6"><br>
+<input type="text" name ="cod_user" placeholder="EX:161384" maxLength="6" required><br>
 </div>
 <br>
 <div class="textfield">
     <label for="numero_user">Numero do Usuário<br></label>
-<input type="text"  name="numero_user" id="" placeholder="EX:(11) 98722-5298"><br>
+<input type="text" name ="numero_user" placeholder="EX:(11) 98722-5298" required><br>
 </div>
 <br>
 <div class="textfield">
     <label for="senha_user">Senha<br></label> 
-<input type="password"  name="senha_user" id="" placeholder="Senha"><br>
+<input type="text" name ="senha_user" placeholder="Senha" required><br>
 </div>
 <br>
 <div class="textfield">
     <label for="senha2_user">Confirme sua Senha<br></label>
-<input type="password"  name="senha2_user" id="" placeholder="Confirme sua Senha"><br> <br>
+<input type="text" name ="senha2_user" placeholder="Confirme sua Senha" required><br> <br>
 </div>
 <label for="dtnasc">Data de Nascimento</label><br>
-<input type="date" id="dtnasc" name="Data Nascimento">
+<input type="date" id="dtnasc" name="Data Nascimento" required>
 <br>
 <br>
-<label>Aceita o Termos de Contrato?</label><input type="checkbox"><br>
+<label arequired>Aceita o Termos de Contrato?</label><input type="checkbox" required><br>
 <br>
-<input clas="btn_conta" type="submit" value="Criar Conta">
+<input class="btn_conta" type="submit" value="Criar Conta">
 </div>
 </form>
