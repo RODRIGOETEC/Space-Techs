02848663330663d09a1f32a7d19a9a62d42a6909
RODRIGOETEC <rodrigo.guedes3@etec.sp.gov.br>
2023-06-02 14:07:58 -0300

    aula 02

--- recomendacao/recomendacao.php
+++ recomendacao/recomendacao.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../estilo/cabecalho.css">
-    <title>Sobre Space Techs</title>
-    <header>
+ 
+    <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="cabecalho.css">
+    <title>Recomendação</title>
+        <header>
     <h1 class="cabecalho">Space-Techs</h1>
     <nav>
     <ul id="menu">
@@ -16,12 +18,63 @@
             <li><a href="recomendacao.php">Recomendações</a></li> 
             <li><a href="../loja/loja.php">Loja</a></li>
             <li><a href="../contato/contato.php">Contato</a></li>
-            </li>        
+            </li>    
         </ul>
 </nav>
-</header>
+    </header>
 </head>
 <body>
+
+
+    <section class="home">
+        <div class="home-img">
+            <img src="home1.jpg" class="one">
+        </div><!--home-img-->
+
+        <div class="home-text">
+
+            <h1>Oshi no Ko</h1>
+            <h5>Ai Hoshino, de 16 anos, é uma linda e talentosa idol que é adorada pelos seus fãs. Ela é o epítome da pureza, mas nem tudo o que reluz é ouro… Gorou Honda é uma ginecologista de uma zona rural e grande fã de Ai. Então, quando a idol grávida aparece no seu hospital, ele fica mais do que perplexo.</h5>
+            <h3>Disponivel em Breve no Disney Plus</h3>
+            <a href="#" class="btn">Veja Mais</a>
+            
+        </div><!--home-text-->
+    </section><!--home-->
+
+
+    <div class="main">
+        <div class="row">
+   <p class="onem" onclick="slider('home1.jpg')">1</p>
+        </div><!--row-->
+
+        <div class="row2">
+        <p onclick="slider('home2.jpg')">2</p>
+        </div><!--row2-->
+
+        <div class="row3">
+            <p onclick="slider('home3.png')">3</p>
+        </div><!--row3-->
+    </div><!--main-->
+
+
+
+
+
+     
+  
+    <script>
+        function slider (anything){
+            document.querySelector ('.one') .src = anything;
+        };
+
+       let menu = document.querySelector ('#menu-icon');
+       let navbar = document.querySelector ('.navbar');
+
+       menu.onclick = () => {
+        menu.classList.toggle ('bx-x');
+        navbar.classList.toggle ('open');
+       }
+    </script>
     
 </body>
 </html>
