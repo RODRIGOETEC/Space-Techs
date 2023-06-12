8388296d88252fdec80f348eb6ef3b1b832d246a
RODRIGOETEC <rodrigo,guedes3@etec.sp.gov.br>
2023-06-12 14:22:48 -0300

    Loja-Noticia

--- loja/loja.php
+++ loja/loja.php
@@ -21,7 +21,104 @@
 </nav>
 </header>
 </head>
+<style>
+        /* Estilos CSS */
+        body{
+            align-items: center;
+            justify-content: center;
+            text-decoration-color: #00ff88;
+            background-color: #2b134b;
+        }
+
+        h1{
+            color: #00ff88;
+            text-align: center;
+            justify-content: center ;
+        }
+
+        .item {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 10px;
+            text-align: center;
+justify-content: center;
+        }
+        #cart {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-top: 10px;
+            text-align: center;
+justify-content: center;
+        }
+
+        .produto{
+            height: 200px;
+            width: 200px;
+        }
+    </style>
+</head>
 <body>
-    
+    <h1>Loja space-Techs</h1>
+    <div id="items">
+        <div class="item">
+            <h2>Camisa do Luff</h2>
+            <img src="luff.jpg" class="produto">
+            <p>Descrição do item 1.</p>
+            <p>Preço: $10</p>
+            <button onclick="addItem('Camisa do Luff', 10)">Adicionar ao Carrinho</button>
+        </div>
+        <div class="item">
+            <h2>Item 2</h2>
+            <p>Descrição do item 2.</p>
+            <p>Preço: $20</p>
+            <button onclick="addItem('Item 2', 20)">Adicionar ao Carrinho</button>
+        </div>
+        <div class="item">
+            <h2>Item 3</h2>
+            <p>Descrição do item 3.</p>
+            <p>Preço: $30</p>
+            <button onclick="addItem('Item 3', 30)">Adicionar ao Carrinho</button>
+        </div>
+         <div class="item">
+            <h2>Item 4</h2>
+            <p>Descrição do item 3.</p>
+            <p>Preço: $70</p>
+            <button onclick="addItem('Item 4', 70)">Adicionar ao Carrinho</button>
+        </div>
+    </div>
+    <div id="cart">
+        <h2>Carrinho</h2>
+        <ul id="cart-items"></ul>
+        <p id="cart-total">Total: $0</p>
+        <input type="submit" name="Comprar" value="Comprar">
+    </div>
+    <script>
+        // Script JavaScript
+        var cartItems = []; // Array para armazenar os itens do carrinho
+
+        function addItem(name, price) {
+            cartItems.push({ name: name, price: price }); // Adiciona o item ao carrinho
+            updateCart(); // Atualiza o carrinho
+        }
+
+        function updateCart() {
+            var cartList = document.getElementById("cart-items");
+            cartList.innerHTML = ""; // Limpa a lista do carrinho
+
+            var total = 0;
+
+            for (var i = 0; i < cartItems.length; i++) {
+                var item = cartItems[i];
+                var listItem = document.createElement("li");
+                listItem.innerHTML = item.name + " - $" + item.price;
+                cartList.appendChild(listItem); // Adiciona o item à lista do carrinho
+
+                total += item.price; // Calcula o total
+            }
+
+            var cartTotal = document.getElementById("cart-total");
+            cartTotal.innerHTML = "Total: $" + total; // Atualiza o total
+        }
+    </script>
 </body>
 </html>
--- noticia/noticia.php
+++ noticia/noticia.php
@@ -21,7 +21,68 @@
 </nav>
 </header>
 </head>
+<style>
+
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #2b134b;
+}
+h1 {
+    margin: 0;
+}
+main {
+    margin: 20px;
+}
+.article {
+    border-bottom: 1px solid #ccc;
+    padding: 20px;
+}
+.article h2 {
+    margin: 0;
+    margin-bottom: 10px;
+}
+.article p {
+    margin: 0;
+}
+.article img {
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 10px;
+}
+.article a {
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+}
+.article a:hover {
+    text-decoration: underline;
+}
+</style>
+</head>
 <body>
-    
+<main>
+<div id="news-list">
+    <div class="article">
+        <h2>Título da Notícia 1</h2>
+        <p>Texto da notícia 1.</p>
+        <img src="imagem1.jpg" alt="Imagem da Notícia 1">
+        <a href="#">Leia mais</a>
+    </div>
+    <div class="article">
+        <h2>Título da Notícia 2</h2>
+        <p>Texto da notícia 2.</p>
+        <img src="imagem2.jpg" alt="Imagem da Notícia 2">
+        <a href="#">Leia mais</a>
+    </div>
+    <div class="article">
+        <h2>Título da Notícia 3</h2>
+        <p>Texto da notícia 3.</p>
+        <img src="imagem3.jpg" alt="Imagem da Notícia 3">
+        <a href="#">Leia mais</a>
+    </div>
+</div>
+</main>
 </body>
 </html>
