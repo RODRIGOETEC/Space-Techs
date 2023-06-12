fababcec157addb3efd036c4678ded49a360b9f5
RODRIGOETEC <rodrigo,guedes3@etec.sp.gov.br>
2023-06-12 15:29:22 -0300

    banco

--- SpaceTechs.sql/cadastrouser.php
+++ SpaceTechs.sql/cadastrouser.php
@@ -4,16 +4,19 @@ $dsn = "mysql:host=localhost;dbname=SpaceTechs";
 $username = "root";
 $password = "";
 $pdo = new PDO($dsn, $username, $password);
+$iduser = '1';
 $nome_user = 'Usuario';
 $sob_user = 'Usuario';
 $name_user = 'Usuario';
 $email_user = 'Usuario';
 $cod_user = 'Usuario';
-$numero_user = 'Usuario';
+$numero_user = '1';
 $senha_user = 'Usuario';
-$nasc_user = 'Usuario';
-$stmt = $pdo->query("insert into usuario values('$nome_user, $sob_user, $name_user, $email_user, $cod_user, $numero_user, $senha_user,$nasc_user')");
-if($stmt){
+//$nasc_user = 'Usuario';
+$query = "INSERT INTO usuario ($iduser, $nome_user, $sob_user, $name_user, $email_user, $cod_user, $numero_user, $senha_user)values";
+
+$stmt = $pdo->prepare($query);
+if($stmt->execute()){
     echo 'usuario criado';
 }else{
     echo 'erro ao inserir usuario';
