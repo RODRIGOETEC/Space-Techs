daebae85c6edbc3c95dee4812514611db42dcc61
jonathas <jonathas.araujo3@etec.sp.gov.br>
2023-06-14 17:50:37 -0300

    Cadastro Noticia

--- cadastro_noticia.php
+++ cadastro_noticia.php
@@ -1,15 +1,61 @@
-<h1>Cadastro Noticias</h1>
+<?php
+// Conectar ao banco de dados
+$dsn = "mysql:host=localhost;dbname=spacetechs";
+$username = "root";
+$password = "";
+$pdo = new PDO($dsn, $username, $password);
 
-Titulo da Noticia<br>
-<input type="text" name="nome_noticia" value="" maxlength="45"> <br> <br>
+// Processar o formulário de cadastro
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $idnoticia = "";
+    $titulo_not = $_POST["titulo_not"];
+    $desc_not = $_POST["desc_not"];
 
-Data da Noticia<br>
-<input id="date" type="date"><br> <br> 
+    // Inserir os dados no banco de dados
+    $query = "INSERT INTO noticia (idnoticia, titulo_not, desc_not) VALUES ('$idnoticia', '$titulo_not', '$desc_not')";
+    $stmt = $pdo->prepare($query);
+    if ($stmt->execute()) {
+        echo "Noticia adastrada com sucesso!";
+    } else {
+        echo "Erro ao cadastrar: " . mysqli_error($pdo);
+    }
+}
 
-Fonte da Noticia<br>
-<input type="text" name="nome_noticia" value="" maxlength="45"> <br> <br> 
 
-Nome do Autor <br>
-<input type="text" name="nome_noticia" value="" maxlength="45"> <br> <br>
+?>
 
-<input type="submit" value="Pesquisar">
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadastro de Usuário</title>
+    <style>
+        body{
+            background-color:#201b2c;
+            text-align: center;
+	justify-content: center;
+	align-items: center;
+    text-decoration-color: #ffffff;
+        }
+        h1{
+            color:#ffffff;
+        }
+
+        label{
+            color:#ffffff;
+        }
+    </style>
+</head>
+<body>
+    <h1>Cadastro de Noticia</h1>
+    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+        <label for="titulo_not">Titulo da Noticia:</label>
+        <input type="text" name="titulo_not" required><br><br>
+
+        <label for="desc_not">Descrição da Noticia:</label>
+        <input type="text" name="desc_not" required><br><br>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
